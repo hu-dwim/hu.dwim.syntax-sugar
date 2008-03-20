baaaf61f1caabf28b4fda06a36b8274f3eab4f4f
attila.lendvai <attila.lendvai@gmail.com>
2008-03-20 11:42:10 +0000

    drop test that tested old behaviour of the readtime-wrapper-syntax

--- tests/quasi-quote.lisp
+++ tests/quasi-quote.lisp
@@ -60,33 +60,3 @@
                  (my-quote (1 2 (my-unquote 3 nil) (my-unquote 4 t) 5))
                  42)))))
 
-(deftest test/quasi-quote/nested-lisp-quasi-quote ()
-  (enable-readtime-wrapper-syntax)
-  (is (equal '(my-quote
-               (1 (my-unquote `(foo ,(+ 40 2)) nil) 3))
-             (read-from-string
-              "{(with-quasi-quote-syntax 'my-quote 'my-unquote)
-                `(1 ,`(foo ,(+ 40 2)) 3)}")))
-  (bind ((expected '(my-quote
-                     (1
-                      (my-quote
-                       (bar (my-unquote
-                             (my-unquote
-                              `(foo ,(+ 40 2))
-                              nil)
-                             nil)))
-                      3))))
-    (is (equal expected
-               (read-from-string
-                "{(with-quasi-quote-syntax 'my-quote 'my-unquote :quasi-quote-character #\\$ :unquote-character #\\;)
-                  $(1 $(bar ;;`(foo ,(+ 40 2))) 3)}")))
-    (is (equal expected
-               (read-from-string
-                "{(with-quasi-quote-syntax 'my-quote 'my-unquote)
-                  `(1 `(bar ,,`(foo ,(+ 40 2))) 3)}")))
-    (is (equal expected
-               (read-from-string
-                "{(with-quasi-quote-syntax 'my-quote 'my-unquote :quasi-quote-character #\\[
-                                                                 :quasi-quote-end-character #\\]
-                                                                 :unquote-character #\\;)
-                  [1 [bar ;;`(foo ,(+ 40 2))] 3]}")))))
