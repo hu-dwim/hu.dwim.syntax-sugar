57fb17ead6f5a37cb342cb1698fde4f60cde624e
attila.lendvai <attila.lendvai@gmail.com>
2008-03-23 15:01:10 +0000

    added missed tests/lambda.lisp

---
+++ tests/lambda.lisp
@@ -0,0 +1,18 @@
+;;; -*- mode: Lisp; Syntax: Common-Lisp; -*-
+;;;
+;;; Copyright (c) 2008 by the authors.
+;;;
+;;; See LICENCE for details.
+
+(in-package :cl-syntax-sugar-test)
+
+(eval-when (:compile-toplevel)
+  (setup-readtable))
+
+(defsuite* (test/lambda :in test))
+
+(deftest test/lambda/installed-on-square-braces ()
+  (is (equal (funcall {(with-lambda-with-bang-args-syntax :start-character #\[ :end-character #\])
+                         [+ !1 40]}
+                      2)
+             42)))
