32f0ae8e5c7068f8aa37ceec9c99b64d063fef75
attila.lendvai <attila.lendvai@gmail.com>
2008-04-06 13:38:18 +0000

    smarter quasi-quote reader, one step towards being able to use for xml

--- src/quasi-quote.lisp
+++ src/quasi-quote.lisp
@@ -11,30 +11,32 @@
 (defvar *quasi-quote-level*)
 
 (define-syntax quasi-quote (quasi-quote-wrapper unquote-wrapper &key
+                                                (nested-quasi-quote-wrapper quasi-quote-wrapper)
                                                 (quasi-quote-character #\`)
                                                 (quasi-quote-end-character nil)
                                                 (unquote-character #\,)
                                                 (splice-character #\@)
-                                                (quasi-quote-reader-wrapper #'identity))
-  (bind ((original-reader-on-quasi-quote-character (multiple-value-list (get-macro-character quasi-quote-character *readtable*)))
-         (original-reader-on-unquote-character     (multiple-value-list (get-macro-character unquote-character *readtable*))))
+                                                (reader-wrapper #'identity))
+  (bind ((original-reader-on-quasi-quote-end-character (when quasi-quote-end-character
+                                                         (multiple-value-list (get-macro-character quasi-quote-end-character *readtable*))))
+         (original-reader-on-unquote-character         (multiple-value-list (get-macro-character unquote-character *readtable*))))
     (set-macro-character quasi-quote-character
-                         (funcall quasi-quote-reader-wrapper
-                                  (make-quasi-quote-reader original-reader-on-quasi-quote-character
+                         (funcall reader-wrapper
+                                  (make-quasi-quote-reader original-reader-on-quasi-quote-end-character
                                                            original-reader-on-unquote-character
-                                                           quasi-quote-character quasi-quote-end-character quasi-quote-wrapper
+                                                           quasi-quote-character quasi-quote-end-character
+                                                           quasi-quote-wrapper nested-quasi-quote-wrapper
                                                            unquote-character unquote-wrapper
                                                            splice-character))
                          t
                          *readtable*)))
 
-(defun make-quasi-quote-reader (original-reader-on-quasi-quote-character
+(defun make-quasi-quote-reader (original-reader-on-quasi-quote-end-character
                                 original-reader-on-unquote-character
-                                quasi-quote-character quasi-quote-end-character quasi-quote-wrapper
+                                quasi-quote-character quasi-quote-end-character
+                                quasi-quote-wrapper nested-quasi-quote-wrapper
                                 unquote-character unquote-wrapper
                                 splice-character)
-  (declare (ignorable original-reader-on-quasi-quote-character
-                      quasi-quote-character))
   (labels ((unquote-reader (stream char)
              (declare (ignore char))
              (bind ((*readtable* (copy-readtable))
@@ -42,24 +44,26 @@
                     (spliced (eq (peek-char nil stream t nil t) splice-character)))
                (when spliced
                  (read-char stream t nil t))
-               (assert (<= 0 *quasi-quote-level*))
+               (assert (>= *quasi-quote-level* 0))
                (when (zerop *quasi-quote-level*)
                  ;; restore the original readers when we are leaving our nesting. this way it's possible
                  ;; to use the ` and , in their normal meanings when being outside our own nesting levels.
-                 ;; (apply 'set-macro-character quasi-quote-character original-reader-on-quasi-quote-character)
-                 ;; this would restore the original start character which would prevent the nested usage
-                 (apply 'set-macro-character unquote-character original-reader-on-unquote-character))
+                 (apply 'set-macro-character unquote-character original-reader-on-unquote-character)
+                 (when quasi-quote-end-character
+                   (apply 'set-macro-character quasi-quote-end-character original-reader-on-quasi-quote-end-character))
+                 (set-macro-character quasi-quote-character #'toplevel-quasi-quote-reader))
                (bind ((body (read stream t nil t)))
                  (if (functionp unquote-wrapper)
                      (funcall unquote-wrapper body spliced)
-                     `(,unquote-wrapper ,body ,spliced)))))
-           (quasi-quote-reader (stream char)
+                     (list unquote-wrapper body spliced)))))
+           (toplevel-quasi-quote-reader (stream char)
              (declare (ignore char))
              (bind ((*quasi-quote-level* (if (boundp '*quasi-quote-level*)
                                              (1+ *quasi-quote-level*)
                                              1))
                     (*readtable* (copy-readtable)))
                (set-macro-character unquote-character #'unquote-reader)
+               (set-macro-character quasi-quote-character #'nested-quasi-quote-reader)
                (bind ((body (if quasi-quote-end-character
                                 ;; we must set the syntax on the end char to be like #\)
                                 ;; until we read out our entire body. this is needed to
@@ -71,6 +75,15 @@
                                 (read stream t nil t))))
                  (if (functionp quasi-quote-wrapper)
                      (funcall quasi-quote-wrapper body)
-                     `(,quasi-quote-wrapper ,body))))))
-    #'quasi-quote-reader))
-
+                     (list quasi-quote-wrapper body)))))
+           (nested-quasi-quote-reader (stream char)
+             (declare (ignore char))
+             (assert (>= *quasi-quote-level* 1))
+             (bind ((*quasi-quote-level* (1+ *quasi-quote-level*))
+                    (body (if quasi-quote-end-character
+                              (read-delimited-list quasi-quote-end-character stream t)
+                              (read stream t nil t))))
+               (if (functionp nested-quasi-quote-wrapper)
+                   (funcall nested-quasi-quote-wrapper body)
+                   (list nested-quasi-quote-wrapper body)))))
+    #'toplevel-quasi-quote-reader))
