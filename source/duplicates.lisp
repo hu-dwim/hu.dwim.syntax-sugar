bbb60e5d8735692cf7fb2893406f033a2a3b380d
levente.meszaros <levente.meszaros@gmail.com>
2009-09-22 13:14:02 +0000

    Eliminate some unused duplicates.

--- source/duplicates.lisp
+++ source/duplicates.lisp
@@ -6,23 +6,6 @@
 
 (in-package :hu.dwim.syntax-sugar)
 
-(defmacro if-bind (var test &body then/else)
-  (assert (first then/else)
-          (then/else)
-          "IF-BIND missing THEN clause.")
-  (destructuring-bind (then &optional else)
-      then/else
-    `(let ((,var ,test))
-       (if ,var ,then ,else))))
-
-(defmacro when-bind (var test &body body)
-  `(if-bind ,var ,test (progn ,@body)))
-
-(defmacro prog1-bind (var ret &body body)
-  `(let ((,var ,ret))
-    ,@body
-    ,var))
-
 ;;; Copied over from Stefil
 
 (define-condition illegal-lambda-list (error)
