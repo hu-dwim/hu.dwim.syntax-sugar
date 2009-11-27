fbb63461c9ad782a962ab02a5b1a30f732812b25
attila.lendvai <attila.lendvai@gmail.com>
2009-11-27 08:57:55 +0000

    feature-cond tests

--- test/feature-cond.lisp
+++ test/feature-cond.lisp
@@ -12,15 +12,19 @@
   `(bind ((*features* (list* ,@features *features*)))
      ,@body))
 
+(defun read-from-string/with-feature-cond-syntax (string)
+  (with-local-readtable
+    (enable-feature-cond-syntax)
+    (read-from-string string)))
+
 (defmacro define-feature-cond-test (name test-form &body tests)
   (with-unique-names (form result position)
     `(deftest ,name ()
-       (enable-feature-cond-syntax)
        (bind ((,form ,test-form))
          ,@(loop
-              :for entry :in tests
+             :for entry :in tests
              :collect `(with-features ,(first entry)
-                         (bind (((:values ,result ,position) (read-from-string ,form)))
+                         (bind (((:values ,result ,position) (read-from-string/with-feature-cond-syntax ,form)))
                            (is (= (length ,form) ,position))
                            (is (equal ,(second entry) ,result)))))))))
 
@@ -58,6 +62,50 @@
   ((:baz) "(and (or :foo (< 5 10)) :baz)")
   ((:bar) "(or :foo :bar t)")
   (() "(or :foo :bar t)"))
+
+(define-feature-cond-test test/feature-cond/evaluation
+    ｢#*((:foo not-evaluated))｣
+  ((:foo) 'not-evaluated)
+  (() nil))
+
+(deftest test/feature-cond/auto-progn-wrapping ()
+  (bind ((form ｢#*((:foo (+ 2 2)
+                         (+ 40 2))
+                   (:bar #. nil
+                         not-evaluated))｣))
+    (with-features (:foo)
+      (bind ((result (read-from-string/with-feature-cond-syntax form)))
+        ;; branches of mutliple forms are automatically wrapped in a `(progn ,@)
+        (is (consp result))
+        (is (eq (first result) 'progn))
+        (is (eql (eval (second result)) 4))
+        (is (eql (eval result) 42))))
+    (with-features (:bar)
+      (bind ((result (read-from-string/with-feature-cond-syntax form)))
+        ;; NIL's are removed from branches of mutliple forms to support #.(warn "careful!") idiom
+        (is (eq result 'not-evaluated))))))
+
+(define-feature-cond-test test/feature-cond/comments
+    ｢#*(; bla1
+        (:foo "it's foo") ; bla2
+        ; bla3
+        (:bar "it's bar") ;bla4
+        )｣
+  ((:foo :bar) "it's foo")
+  ((:bar) "it's bar")
+  (() nil))
+
+(deftest test/feature-cond/conditions ()
+  (bind ((form ｢#*((:foo "it's foo")
+                   (:bar "it's bar")
+                   (t #.(warn "default reached")))｣))
+    (with-features (:foo :bar)
+      (not-signals condition
+        (is (string= (read-from-string/with-feature-cond-syntax form)
+                     "it's foo"))))
+    (bind ((condition (signals simple-warning (read-from-string/with-feature-cond-syntax form))))
+      (when condition
+        (is (string= (simple-condition-format-control condition) "default reached"))))))
 
 (define-feature-cond-test test/feature-cond/bug/1
     ｢#*((:missing 'non-existent-package::zork)
